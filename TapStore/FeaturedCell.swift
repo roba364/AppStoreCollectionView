e5553d231fead956e2b79e6cf5454d57cbbc1b48
Sergey <borovkovsergey364@gmail.com>
2020-05-27 15:46:15 +0300

    - realized FeaturedCell

---
+++ TapStore/FeaturedCell.swift
@@ -0,0 +1,65 @@
+//
+//  FeaturedCell.swift
+//  TapStore
+//
+//  Created by SofiaBuslavskaya on 27/05/2020.
+//  Copyright © 2020 Hacking with Swift. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedCell: UICollectionViewCell, SelfConfiguringCell {
+    
+    //MARK: - Properties
+    
+    static var reuseIdentifier: String = "FeaturedCell"
+    
+    let tagline = UILabel()
+    let name = UILabel()
+    let subtitle = UILabel()
+    let imageView = UIImageView()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        tagline.font = UIFontMetrics.default.scaledFont(for: UIFont.systemFont(ofSize: 12, weight: .bold))
+        tagline.textColor = .systemBlue
+        
+        name.font = UIFont.preferredFont(forTextStyle: .title2)
+        name.textColor = .label
+        
+        subtitle.font = UIFont.preferredFont(forTextStyle: .title2)
+        subtitle.textColor = .secondaryLabel
+        
+        imageView.layer.cornerRadius = 5
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        
+        let stackView = UIStackView(arrangedSubviews: [tagline, name, subtitle, imageView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        stackView.setCustomSpacing(10, after: subtitle)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) FeaturedCell has not been implemented")
+    }
+    
+    func configure(with app: App) {
+        tagline.text = app.tagline.uppercased()
+        name.text = app.name
+        imageView.image = UIImage(named: app.image)
+    }
+    
+}
